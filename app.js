039f7d7efb236897e17c71c900b396e45fd17037
roypyp <roypyp@gmail.com>
2021-04-20 17:58:18 +0300

    enemy

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 var context;
 var shape = new Object();
+var enemypos =new Object();
 var board;
+var lifepool;
 var score;
 var pac_color;
 var pac_pos=2;
 var start_time;
 var time_elapsed;
 var interval;
+var interval2;
 var temp=true;
 var dic_users = {'k':'k'}
+var enemy=new Image();
 
+
+
+
+
+
+enemy.src="enemy.png";
 
 $(document).ready(function() {
+	
+
 
 	//Check the Validiation of Login form
 	$('#log_form').submit(function(e){
@@ -112,6 +124,8 @@ $(document).ready(function() {
 });
 
 
+
+
 function accept(){
 	var reg=document.getElementById("Register");
 	reg.style.display="none";
@@ -123,6 +137,7 @@ function accept(){
 function welcome(){
 //1.page cleaning
 	window.clearInterval(interval);
+	window.clearInterval(interval2);
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.beginPath();
 //2.	
@@ -182,14 +197,18 @@ function Login(){
 
 function Start() {
 	board = new Array();
+	board2 =new Array();
 	score = 0;
 	pac_color = "yellow";
+	lifepool=1
 	var cnt = 100;
 	var food_remain = 50;
 	var pacman_remain = 1;
+	var enemy_remain = 1;
 	start_time = new Date();
 	for (var i = 0; i < 10; i++) {
 		board[i] = new Array();
+		board2[i] = new Array();
 		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
 		for (var j = 0; j < 10; j++) {
 			if (
@@ -200,18 +219,28 @@ function Start() {
 				(i == 6 && j == 2)
 			) {
 				board[i][j] = 4;
+				board2[i][j] = 4;
 			} else {
 				var randomNum = Math.random();
 				if (randomNum <= (1.0 * food_remain) / cnt) {
 					food_remain--;
 					board[i][j] = 1;
+					board2[i][j] = 0;
 				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
 					shape.i = i;
 					shape.j = j;
 					pacman_remain--;
 					board[i][j] = 2;
+					board2[i][j] = 2;
+				}else if (randomNum < (1.0 * ( enemy_remain + food_remain)) / cnt) {
+					enemypos.i=i;
+					enemypos.j=j;
+					enemy_remain--;
+					board[i][j] = 0;
+					board2[i][j] = 5;
 				} else {
 					board[i][j] = 0;
+					board2[i][j] = 0;
 				}
 				cnt--;
 			}
@@ -237,7 +266,8 @@ function Start() {
 		},
 		false
 	);
-	interval = setInterval(UpdatePosition, 250);
+	interval = setInterval(UpdatePosition, 100);
+	interval2 = setInterval(UpdateEnemyPosition, 450);
 }
 
 function findRandomEmptyCell(board) {
@@ -274,7 +304,19 @@ function Draw() {
 			var center = new Object();
 			center.x = i * 60 + 30;
 			center.y = j * 60 + 30;
-			if (board[i][j] == 2) {//right
+			if (board2[i][j] == 5) 
+			{//enemy
+					context.beginPath();
+					context.drawImage(enemy,center.x-20,center.y-20);
+					//context.fillStyle = "grey"; //color
+					//context.fill();
+				//context.beginPath();
+				//context.arc(center.x, center.y, 30, 0, 2 * Math.PI);
+				//context.fillStyle = "red"; //color
+				//context.fill();
+			}
+			else if (board[i][j] == 2) 
+			{//right
 				context.beginPath();
 				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
 				context.lineTo(center.x, center.y);
@@ -283,8 +325,10 @@ function Draw() {
 				context.beginPath();
 				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
 				context.fillStyle = "black"; //color
-				context.fill();}
-			else if (board[i][j] == 2.1) {//left
+				context.fill();
+			}
+			else if (board[i][j] == 2.1) 
+			{//left
 				context.beginPath();
 				context.arc(center.x, center.y, 30,  1.15 * Math.PI,0.85* Math.PI); // half circle
 				context.lineTo(center.x, center.y);
@@ -295,7 +339,8 @@ function Draw() {
 				context.fillStyle = "black"; //color
 				context.fill();
 			}
-			else if (board[i][j] == 2.2) {//down
+			else if (board[i][j] == 2.2) 
+			{//down
 				context.beginPath();
 				context.arc(center.x, center.y, 30,  0.65 * Math.PI,2.35* Math.PI); // half circle
 				context.lineTo(center.x, center.y);
@@ -306,7 +351,8 @@ function Draw() {
 				context.fillStyle = "black"; //color
 				context.fill();
 			}
-			else if (board[i][j] == 2.3) {//up
+			else if (board[i][j] == 2.3) 
+			{//up
 				context.beginPath();
 				context.arc(center.x, center.y, 30,   1.65 * Math.PI,1.35* Math.PI); // half circle
 				context.lineTo(center.x, center.y);
@@ -317,17 +363,20 @@ function Draw() {
 				context.fillStyle = "black"; //color
 				context.fill();
 			}
-			 else if (board[i][j] == 1) {
+			 else if (board[i][j] == 1) 
+			 {//food
 				context.beginPath();
 				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
 				context.fillStyle = "black"; //color
 				context.fill();
-			} else if (board[i][j] == 4) {
+			} else if (board[i][j] == 4) 
+			{//wall
 				context.beginPath();
 				context.rect(center.x - 30, center.y - 30, 60, 60);
 				context.fillStyle = "grey"; //color
 				context.fill();
 			}
+			
 		}
 	}
 }
@@ -336,43 +385,166 @@ function UpdatePosition() {
 	board[shape.i][shape.j] = 0;
 	var x = GetKeyPressed();
 	if (x == 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
+		pac_pos = 2.3;
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
 			shape.j--;
-			pac_pos = 2.3;
+			
 		}
 	}
 	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
+		pac_pos = 2.2;
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
 			shape.j++;
-			pac_pos = 2.2;
+			
 		}
 	}
 	if (x == 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
+		pac_pos = 2.1;
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
 			shape.i--;
-			pac_pos = 2.1;
+			
 		}
 	}
 	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
+		pac_pos= 2;
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
 			shape.i++;
-			pac_pos= 2;
+			
 		}
 	}
-	if (board[shape.i][shape.j] == 1) {
+	if(board2[shape.i][shape.j] == 5){
+		lifepool--;
+		score-=10
+	}
+	else if (board[shape.i][shape.j] == 1) {
 		score++;
 	}
+
 	board[shape.i][shape.j]=pac_pos
 	var currentTime = new Date();
 	time_elapsed = (currentTime - start_time) / 1000;
 	if (score >= 20 && time_elapsed <= 10) {
 		pac_color = "green";
 	}
+	if(lifepool==0){
+		window.clearInterval(interval);
+		window.clearInterval(interval2);
+		window.alert("game over");
+		welcome();
+	}
 	if (score == 50 ) {
 		window.clearInterval(interval);
+		window.clearInterval(interval2);
 		window.alert("Game completed");
 		welcome();
 	} else {
 		Draw();
 	}
+	
+}
+
+function UpdateEnemyPosition(){
+	board2[enemypos.i][enemypos.j] = 0;
+	var x=getBestMove();
+	if (x == 1) {
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (enemypos.j > 0 && board[enemypos.i][enemypos.j - 1] != 4) {
+			enemypos.j--;
+			
+		}
+	}
+	if (x == 2) {
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (enemypos.j < 9 && board[enemypos.i][enemypos.j + 1] != 4) {
+			enemypos.j++;
+			
+		}
+	}
+	if (x == 3) {
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (enemypos.i > 0 && board[enemypos.i - 1][enemypos.j] != 4) {
+			enemypos.i--;
+			
+		}
+	}
+	if (x == 4) {
+		if (shape.i == enemypos.i && shape.j == enemypos.j){
+			lifepool--;
+			score-=10
+		}
+		else if (enemypos.i < 9 && board[enemypos.i + 1][enemypos.j] != 4) {
+			enemypos.i++;
+			
+		}
+	}
+	if(board[enemypos.i][enemypos.j] == 2){
+		lifepool--;
+		score-=10
+	}
+
+
+	board2[enemypos.i][enemypos.j]=5
+	var currentTime = new Date();
+	time_elapsed = (currentTime - start_time) / 1000;
+	if(lifepool==0){
+		window.clearInterval(interval);
+		window.clearInterval(interval2);
+		window.alert("game over");
+		welcome();
+	}
+	if (score == 50 ) {
+		window.clearInterval(interval);
+		window.clearInterval(interval2);
+		window.alert("Game completed");
+		welcome();
+	} else {
+		Draw();
+	}
+}
+function getBestMove(){
+	var a=[100,100,100,100];
+	var b=0;
+	var c=100;
+	if (enemypos.j > 0 && board[enemypos.i][enemypos.j - 1] != 4){
+		a[0]=Math.abs(enemypos.i-shape.i) + Math.abs((enemypos.j-1)-shape.j);
+		if(a[0]<c) {b=1;c=a[0]}
+	}
+	if (enemypos.j < 9 && board[enemypos.i][enemypos.j + 1] != 4) {
+		a[1]=Math.abs(enemypos.i-shape.i) + Math.abs((enemypos.j+1)-shape.j);
+		if(a[1]<c) {b=2;c=a[1]}
+	}
+	if (enemypos.i > 0 && board[enemypos.i - 1][enemypos.j] != 4) {
+		a[2]=Math.abs((enemypos.i-1)-shape.i) + Math.abs(enemypos.j-shape.j);
+		if(a[2]<c) {b=3;c=a[2]}
+	}
+	if (enemypos.i < 9 && board[enemypos.i + 1][enemypos.j] != 4) {
+		a[3]=Math.abs((enemypos.i+1)-shape.i) + Math.abs(enemypos.j-shape.j);
+		if(a[3]<c) {b=4;c=a[3]};
+	}
+	return b
 }
